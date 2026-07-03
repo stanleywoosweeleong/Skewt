11e39242f6c80f4b38eb8e978bf52d80691e4e37
stanleywoosweeleong <130345527+stanleywoosweeleong@users.noreply.github.com>
2026-07-03 13:22:15 +0800

    bump cache versions

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    app fully usable with no connection.
    Bumping CACHE_VERSION is still good hygiene (it purges old caches on activate),
    but is no longer required for users to receive updates. */
-const CACHE_VERSION = "skewt-v49-buildtag";
+const CACHE_VERSION = "skewt-v50-uxfix";
 
 self.addEventListener("install", e => self.skipWaiting());
 
