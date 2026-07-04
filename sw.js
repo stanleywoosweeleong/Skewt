3095f62dfa488d55f97b69d1f7fdc3baa783be64
stanleywoosweeleong <130345527+stanleywoosweeleong@users.noreply.github.com>
2026-07-04 17:08:19 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    app fully usable with no connection.
    Bumping CACHE_VERSION is still good hygiene (it purges old caches on activate),
    but is no longer required for users to receive updates. */
-const CACHE_VERSION = "skewt-v70-farmpicker";
+const CACHE_VERSION = "skewt-v77-seeddot";
 
 self.addEventListener("install", e => self.skipWaiting());
 
